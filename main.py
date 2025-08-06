fe675d527f86406a344253a20351235663d11bf0
Aleksandr Yurin <myrzik0607@gmail.com>
2025-08-06 15:43:49 +0500

    Fix webhook method and update text

--- main.py
+++ main.py
@@ -10,23 +10,24 @@ from handlers import user, admin  # Подключаем роутеры из han
 
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher(storage=MemoryStorage())
 
-# Обработчик для корня, чтобы проверить, что сервер работает
+# Обработчик для корня
 async def on_start(request):
     return web.Response(text="Bot is running!")
 
 async def main():
     dp.include_router(user.router)  # Включаем роутер из user.py
     dp.include_router(admin.router)  # Включаем роутер из admin.py (если есть)
-
+    
     # Настройка webhook
     app = web.Application()
-    app.add_routes([web.post(f"/{TOKEN}", dp.start_webhook)])
+    app.add_routes([web.post(f"/{TOKEN}", dp.handle_webhook)])
 
-    # Запуск вебхука через aiohttp
+    # Запуск веб-сервера через aiohttp
     await web.run_app(app, host="0.0.0.0", port=3000)
 
 if __name__ == "__main__":
